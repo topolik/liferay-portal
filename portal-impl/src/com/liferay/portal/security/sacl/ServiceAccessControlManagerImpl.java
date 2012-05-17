2b5933fe2c8aae29ffa5f6297292f195cae7eac9
Michael Han <michael.han@liferay.com>
2012-05-17 12:02:19 +0900

    add service acl for securing methods

---
+++ portal-impl/src/com/liferay/portal/security/sacl/ServiceAccessControlManagerImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sacl;
+
+import com.liferay.portal.security.RemoteMethodAccessType;
+import com.liferay.portal.security.MethodSecurity;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.security.permission.PermissionThreadLocal;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Tomas Polesovsky
+ * @author Igor Spasic
+ * @author Michael C. Han
+ */
+public class ServiceAccessControlManagerImpl {
+	public void accept(Method method, MethodSecurity methodSecurity)
+		throws SecurityException {
+
+		if (methodSecurity.remoteMethodAccessType() == RemoteMethodAccessType.AUTHENTICATED) {
+			PermissionChecker permissionChecker =
+				PermissionThreadLocal.getPermissionChecker();
+
+			if ((permissionChecker == null) ||
+				!permissionChecker.isSignedIn()) {
+
+				throw new SecurityException(
+					"Authenticated access required.");
+			}
+		}
+	}
+
+}
