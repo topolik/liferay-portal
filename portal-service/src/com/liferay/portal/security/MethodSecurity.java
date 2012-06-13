f31a30c0fee5c77c04ee0419033acb5b212b5a50
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2012-06-13 11:38:14 +0200

    formatting

--- portal-service/src/com/liferay/portal/security/MethodSecurity.java
+++ portal-service/src/com/liferay/portal/security/MethodSecurity.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -29,6 +29,6 @@ import java.lang.annotation.Target;
 public @interface MethodSecurity {
 
 	RemoteMethodAccessType remoteMethodAccessType()
-	    default RemoteMethodAccessType.AUTHENTICATED;
+		default RemoteMethodAccessType.AUTHENTICATED;
 
 }
--- portal-service/src/com/liferay/portal/security/RemoteMethodAccessType.java
+++ portal-service/src/com/liferay/portal/security/RemoteMethodAccessType.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -19,4 +19,5 @@ package com.liferay.portal.security;
  */
 public enum RemoteMethodAccessType {
 	ANONYMOUS, AUTHENTICATED
+
 }
