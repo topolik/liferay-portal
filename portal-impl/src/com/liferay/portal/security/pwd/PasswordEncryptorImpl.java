2fd135f13cad5b83cda15851004ed72b4acdb9b4
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2013-03-11 14:58:59 +0100

    LPS-33511 Simplify PBKDF2 implementation

--- portal-impl/src/com/liferay/portal/security/pwd/PasswordEncryptorImpl.java
+++ portal-impl/src/com/liferay/portal/security/pwd/PasswordEncryptorImpl.java
@@ -84,12 +84,11 @@ public class PasswordEncryptorImpl implements PasswordEncryptor {
 		if (Validator.isNull(currentEncryptedPassword)) {
 			int rounds = _DEFAULT_BCRYPT_ROUNDS;
 
-			Matcher algorithmRoundsMatcher = _algorithmRounds.matcher(
-				algorithm);
+			Matcher bcryptMatcher = _BCRYPT_PATTERN.matcher(algorithm);
 
-			if (algorithmRoundsMatcher.matches()) {
+			if (bcryptMatcher.matches()) {
 				rounds = GetterUtil.getInteger(
-					algorithmRoundsMatcher.group(1), _DEFAULT_BCRYPT_ROUNDS);
+					bcryptMatcher.group(1), _DEFAULT_BCRYPT_ROUNDS);
 			}
 
 			salt = BCrypt.gensalt(rounds);
@@ -106,41 +105,57 @@ public class PasswordEncryptorImpl implements PasswordEncryptor {
 			String currentEncryptedPassword)
 		throws PwdEncryptorException {
 
-		byte[] saltBytes = _getSaltFromPBKDF2(
-			algorithm, currentEncryptedPassword);
-
-		if (saltBytes.length < 12) {
-			throw new PwdEncryptorException("Unsupported salt length");
-		}
-
+		byte[] salt = new byte[_DEFAULT_PBKDF2_SALT_SIZE];
 		int keySize = _DEFAULT_PBKDF2_KEY_SIZE;
+		int rounds = _DEFAULT_PBKDF2_ROUNDS;
 
-		int slashIndex = algorithm.indexOf(CharPool.SLASH);
-		if (slashIndex > -1) {
-			int startIndex = slashIndex + 1;
-			int endIndex = algorithm.indexOf(CharPool.SLASH, startIndex);
+		if (Validator.isNull(currentEncryptedPassword)) {
+			Matcher pbkdf2Matcher = _PBKDF2_PATTERN.matcher(algorithm);
 
-			if (endIndex > startIndex) {
+			if (pbkdf2Matcher.matches()) {
 				keySize = GetterUtil.getInteger(
-					algorithm.substring(startIndex, endIndex), keySize);
+					pbkdf2Matcher.group(1), _DEFAULT_PBKDF2_KEY_SIZE);
+
+				rounds = GetterUtil.getInteger(
+					pbkdf2Matcher.group(2), _DEFAULT_PBKDF2_ROUNDS);
 			}
 
-			algorithm = algorithm.substring(0, slashIndex);
+			SecureRandom random = new SecureRandom();
+			random.nextBytes(salt);
+		}
+		else {
+			byte[] configAndSalt = new byte[16];
+			try {
+				byte[] passwordBytes = Base64.decode(currentEncryptedPassword);
+
+				System.arraycopy(
+					passwordBytes, 0, configAndSalt, 0, configAndSalt.length);
+			}
+			catch (Exception e) {
+				throw new PwdEncryptorException(
+					"Unable to extract salt from encrypted password: " +
+						e.getMessage());
+			}
+
+			ByteBuffer buff = ByteBuffer.wrap(configAndSalt);
+			keySize = buff.getInt();
+			rounds = buff.getInt();
+			buff.get(salt);
 		}
 
-		byte[] onlySaltBytes = new byte[saltBytes.length - 4];
-
-		ByteBuffer buff = ByteBuffer.wrap(saltBytes);
-		int rounds = buff.getInt();
-		buff.get(onlySaltBytes);
-
 		PBEKeySpec keySpec = new PBEKeySpec(
-			clearTextPassword.toCharArray(), onlySaltBytes, rounds, keySize);
+			clearTextPassword.toCharArray(), salt, rounds, keySize);
 
 		byte[] key;
 		try {
+			String algorithmName = algorithm;
+			int slashIndex = algorithm.indexOf(CharPool.SLASH);
+			if (slashIndex > -1) {
+				algorithmName = algorithm.substring(0, slashIndex);
+			}
+
 			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(
-				algorithm);
+				algorithmName);
 
 			key = keyFactory.generateSecret(keySpec).getEncoded();
 		} catch (InvalidKeySpecException e) {
@@ -151,69 +166,24 @@ public class PasswordEncryptorImpl implements PasswordEncryptor {
 				"Unable to generate hash: " + e.getMessage(), e);
 		}
 
-		ByteBuffer result = ByteBuffer.allocate(saltBytes.length + key.length);
-		result.put(saltBytes);
+		ByteBuffer result = ByteBuffer.allocate(2*4 + salt.length + key.length);
+		result.putInt(keySize);
+		result.putInt(rounds);
+		result.put(salt);
 		result.put(key);
 		return Base64.encode(result.array());
 	}
 
-	private byte[] _getSaltFromPBKDF2(String algorithm, String pbkdf2String)
-		throws PwdEncryptorException {
-
-		byte[] roundsWithSalt = new byte[12];
-
-		if (Validator.isNull(pbkdf2String)) {
-			byte[] saltBytes = new byte[8];
-
-			SecureRandom random = new SecureRandom();
-
-			random.nextBytes(saltBytes);
-
-			int rounds = _DEFAULT_PBKDF2_ROUNDS;
-
-			Matcher algorithmRoundsMatcher = _algorithmRounds.matcher(
-				algorithm);
-
-			if (algorithmRoundsMatcher.matches()) {
-				rounds = GetterUtil.getInteger(
-					algorithmRoundsMatcher.group(1), rounds);
-			}
-
-			ByteBuffer buff = ByteBuffer.allocate(12);
-			buff.putInt(rounds);
-			buff.put(saltBytes);
-
-			roundsWithSalt = buff.array();
-
-			return roundsWithSalt;
-		}
-		else {
-			try {
-				byte[] saltPlusDigest = Base64.decode(pbkdf2String);
-
-				System.arraycopy(
-					saltPlusDigest, 0, roundsWithSalt, 0,
-					roundsWithSalt.length);
-
-				return roundsWithSalt;
-			}
-			catch (Exception e) {
-				throw new PwdEncryptorException(
-					"Unable to extract salt from encrypted password: " +
-						e.getMessage());
-			}
-		}
-	}
-
-	private static final Pattern _algorithmRounds = Pattern.compile(
-		"^.*/([0-9]+)$");
-
+	private static final Pattern _BCRYPT_PATTERN = Pattern.compile(
+		"^BCrypt/([0-9]+)$", Pattern.CASE_INSENSITIVE);
 	private static final int _DEFAULT_BCRYPT_ROUNDS = 10;
 	private static final int _DEFAULT_PBKDF2_KEY_SIZE = 160;
 	private static final int _DEFAULT_PBKDF2_ROUNDS = 128000;
-
+	private static final int _DEFAULT_PBKDF2_SALT_SIZE = 8;
 	private static final String _PASSWORDS_ENCRYPTOR_LIFERAY_ALGORITHM =
 		PropsUtil.get("passwords.encryptor.liferay.algorithm");
+	private static final Pattern _PBKDF2_PATTERN = Pattern.compile(
+		"^.*/?([0-9]+)?/([0-9]+)$");
 
 	private PasswordEncryptor _parent;
 
