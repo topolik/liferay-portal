7d98f532ef0fdbecc799e7e5d32362a06b2a8aa5
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2014-04-22 16:28:51 +0200

    Code cleaning

--- osgi/modules/jax-ws-osgi/src/main/java/com/liferay/portal/service/osgi/Activator.java
+++ osgi/modules/jax-ws-osgi/src/main/java/com/liferay/portal/service/osgi/Activator.java
@@ -6,33 +6,22 @@ import com.liferay.portal.service.osgi.cxf.externalpolicy.LiferayExternalAttachm
 import com.liferay.portal.service.remote.JAXWSServiceImpl;
 import com.liferay.portal.service.remote.JAXWSServiceImpl1;
 import com.liferay.portal.service.remote.JAXWSServiceImpl2;
-import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.jaxws22.spi.ProviderImpl;
-import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
-import org.apache.cxf.ws.policy.WSPolicyFeature;
-import org.apache.cxf.ws.policy.attachment.external.DomainExpressionBuilderRegistry;
 //import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 //import org.apache.ws.security.handler.WSHandlerConstants;
 //import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
 //import org.apache.wss4j.dom.handler.WSHandlerConstants;
-import org.apache.cxf.ws.security.policy.WSSecurityPolicyLoader;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.xml.sax.SAXException;
 
-import javax.servlet.ServletRegistration;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.ws.Endpoint;
-import javax.xml.ws.spi.Provider;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -40,266 +29,132 @@ import java.util.Map;
  * @author Tomas Polesovsky
  */
 public class Activator implements BundleActivator {
-
-	private static final String WSU_NS
-		= "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
-
-	List<Endpoint> endpoints;
-	Server httpServer;
-	BundleContext _bundleContext;
-
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-try {
-		_bundleContext = bundleContext;
+		try {
+			_bundleContext = bundleContext;
 
-//		_bundleContext.registerService(JAXWSService.class.getName(), new JAXWSServiceImpl(), null);
+			_cxfServlet = new LiferayCXFOSGiServlet();
 
-//		Endpoint.publish("http://localhost:9000/helloWorld", new JAXWSServiceImpl());
-		_cxfServlet = new LiferayCXFOSGiServlet();
+			initHTTPServer();
 
-		initHTTPServer();
-
-		LiferayExternalAttachmentProvider externalAttachmentProvider = new LiferayExternalAttachmentProvider(_cxfServlet.getBus());
-		externalAttachmentProvider.setLocation(getClass().getResource("/policy-attachment.xml"));
-//
-//		org.apache.jcp.xml.dsig.internal.dom.DOMKeyInfoFactory factory = null;
-//		if(factory != null){
-//			System.out.println("never reached");
-//		}
-//
-//		try {
-			initCXFEnv();
-//		} catch (Exception e) {
-//			stop(_bundleContext);
-//			e.printStackTrace();
-//		}
-} catch (Exception e) {
-	e.printStackTrace();
-}
+			try {
+				initCXFEnv();
+			} catch (Exception e) {
+				stop(_bundleContext);
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	@Override
+	public void stop(BundleContext bundleContext) {
+		if (_endpoints != null) {
+			for (Endpoint endpoint : _endpoints) {
+				endpoint.stop();
+			}
+			_endpoints = null;
+		}
+
+		if (_httpServer != null) {
+			try {
+				_httpServer.stop();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		_httpServer = null;
+	}
+
+	protected void initCXFEnv() throws Exception {
+
+		_endpoints = new ArrayList<Endpoint>();
+
+		// register our Bus instance into ThreadLocal for the provider
+		BusFactory.setThreadDefaultBus(_cxfServlet.getBus());
+
+		// bad bad ugly thing - call private CXF API to get Provider
+		ProviderImpl provider = new ProviderImpl();
+
+		// another bad bad ugly thing
+		// CXF's WSPolicyFeature is a package-private class and cannot be used
+		// externally, only from Spring XML config :(
+		LiferayWSPolicyFeature wsPolicyFeature = new LiferayWSPolicyFeature();
+
+		// LiferayExternalAttachmentProvider ...
+		// Copy of CXF's ExternalAttachmentProvider adapted to non-Spring env
+		// Registers itself as a policy provider for this CXF bus
+		LiferayExternalAttachmentProvider externalAttachmentProvider =
+			new LiferayExternalAttachmentProvider(_cxfServlet.getBus());
+		externalAttachmentProvider.setLocation(
+			getClass().getResource("/policy-attachment.xml"));
+
+		Endpoint endpoint;
+
+		/*
+		 * A service using WS-Policy using annotations with specific WS-Policy
+		 * files
+		 */
+		JAXWSServiceImpl serviceImpl = new JAXWSServiceImpl();
+		endpoint = provider.createEndpoint(null, serviceImpl);
+		addEndpointProperties(endpoint, wsPolicyFeature);
+		endpoint.publish("/service");
+		_endpoints.add(endpoint);
+
+		/*
+		 * A service using PortalWSPolicy class to find & load WS-Policy file
+		 */
+		JAXWSServiceImpl1 serviceImpl1 = new JAXWSServiceImpl1();
+		endpoint = provider.createEndpoint(null, serviceImpl1);
+		addEndpointProperties(endpoint, wsPolicyFeature);
+		endpoint.publish("/service1");
+		_endpoints.add(endpoint);
+
+		/*
+		 * A service that is configured using LiferayExternalAttachmentProvider,
+		 * there is no direct reference to WS-Policy from the service itself
+		 *  => WIN!
+		 */
+		JAXWSServiceImpl2 serviceImpl2 = new JAXWSServiceImpl2();
+		endpoint = provider.createEndpoint(null, serviceImpl2);
+		addEndpointProperties(endpoint, wsPolicyFeature);
+		endpoint.publish("/service2");
+		_endpoints.add(endpoint);
+	}
+
 
 	protected void initHTTPServer() throws Exception {
 		ServletHolder servlet = new ServletHolder(_cxfServlet);
 		servlet.setName("soap");
 		servlet.setForcedPath("soap");
 
-		httpServer = new Server(11112);
+		_httpServer = new Server(11112);
 		ContextHandlerCollection contexts = new ContextHandlerCollection();
-		httpServer.setHandler(contexts);
+		_httpServer.setHandler(contexts);
 		ServletContextHandler root = new ServletContextHandler(contexts, "/", ServletContextHandler.SESSIONS);
 
 		root.addServlet(servlet, "/soap/*");
 
-		httpServer.start();
+		_httpServer.start();
 	}
 
-	protected void initCXFEnv() throws Exception {
-
-		endpoints = new ArrayList<Endpoint>();
-
-		// register our Bus instance for the provider
-		BusFactory.setThreadDefaultBus(_cxfServlet.getBus());
-
-		ProviderImpl provider = new ProviderImpl();
-
-		Endpoint endpoint;
-
-
-		JAXWSServiceImpl serviceImpl = new JAXWSServiceImpl();
-		endpoint = provider.createEndpoint(null, serviceImpl);
-		addEndpointProperties(endpoint);
-		endpoint.publish("/service");
-		endpoints.add(endpoint);
-
-
-		JAXWSServiceImpl1 serviceImpl1 = new JAXWSServiceImpl1();
-		endpoint = provider.createEndpoint(null, serviceImpl1);
-		addEndpointProperties(endpoint);
-		endpoint.publish("/service1");
-		endpoints.add(endpoint);
-
-		JAXWSServiceImpl2 serviceImpl2 = new JAXWSServiceImpl2();
-		endpoint = provider.createEndpoint(null, serviceImpl2);
-		addEndpointProperties(endpoint);
-
-		LiferayWSPolicyFeature wsPolicyFeature = new LiferayWSPolicyFeature();
+	protected void addEndpointProperties(Endpoint endpoint, LiferayWSPolicyFeature wsPolicyFeature) {
+		// register WS-Policy to be used with the endpoint
 		((EndpointImpl) endpoint).getFeatures().add(wsPolicyFeature);
 
-		endpoint.publish("/service2");
-		endpoints.add(endpoint);
-
-
-//			//**** Policy ****
-//			Policy policy = cxf.getBus().getExtension(PolicyBuilder.class).
-//				getPolicy(getClass().getResourceAsStream("/test-policy.xml"));
-
-
-
-//			((EndpointImpl) endpoint).getServerFactory().getFeatures().add(new WSPolicyFeature(policy));
-//			((EndpointImpl) endpoint).getServiceFactory().getFeatures().add(new WSPolicyFeature(policy));
-//			((EndpointImpl) endpoint).getFeatures().add(new WSPolicyFeature(policy));
-
-//			addSecurity(endpoint, cxf.getBus());
-
-	}
-
-	protected void addEndpointProperties(Endpoint endpoint) {
-		// *** jax-ws security configuration ***
+		// WSS4J security configuration for JAX-WS
 		Map<String, Object> jaxWSProps = endpoint.getProperties();
 		jaxWSProps.put("ws-security.callback-handler", new ServiceKeystorePasswordCallback());
 		jaxWSProps.put("ws-security.encryption.properties", "serviceKeystore.properties");
 		jaxWSProps.put("ws-security.signature.properties", "serviceKeystore.properties");
-//		jaxWSProps.put("ws-security.encryption.username", "useReqSigCert");
 		jaxWSProps.put("ws-security.encryption.username", "myservicekey");
-
 	}
 
-	@Override
-	public void stop(BundleContext bundleContext) {
-		if (endpoints != null) {
-			for (Endpoint endpoint : endpoints) {
-				endpoint.stop();
-			}
-			endpoints = null;
-		}
-
-		if (httpServer != null) {
-			try {
-				httpServer.stop();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		httpServer = null;
-	}
-//
-//	protected void addSecurity(Endpoint endpoint, Bus bus) throws ParserConfigurationException, SAXException, IOException {
-//
-////		boolean wsSecPolicy = true;
-////		if(wsSecPolicy) {
-////			Map<String, Object> properties = endpoint.getProperties();
-////
-////			properties.put("ws-security.callback-handler", new ServiceKeystorePasswordCallback());
-////			properties.put("ws-security.encryption.properties", "serviceKeystore.properties");
-////			properties.put("ws-security.signature.properties", "serviceKeystore.properties");
-////			properties.put("ws-security.encryption.username", "useReqSigCert");
-////
-////			return;
-////		}
-//
-//		if (!(endpoint instanceof EndpointImpl)) {
-//			System.out.println("Not an CXF endpoint!");
-//		}
-//
-//		EndpointImpl endpointImpl = (EndpointImpl) endpoint;
-//		org.apache.cxf.endpoint.Endpoint cxfEndpoint =
-//			endpointImpl.getServer().getEndpoint();
-//
-///////////////////////////
-////
-////		PolicyEngine pe = bus.getExtension(PolicyEngine.class);
-////		if(pe == null || !pe.isEnabled()) {
-////			throw new RuntimeException("PolicyEngine is not available!");
-////		}
-////
-////		Policy policy = bus.getExtension(PolicyBuilder.class).getPolicy(getClass().getResourceAsStream("/message-out-policy.xml"));
-////		EndpointPolicy pe = new EndpointPolicyImpl(policy, pe, false, null);
-////
-////		pe.setServerEndpointPolicy(cxfEndpoint.getEndpointInfo(), new EndpointPolicyImpl(policy));
-////
-////		if(true)return;
-//////////////////////////////
-//
-//		// check incoming messages
-//		Map<String,Object> inProps= new HashMap<String,Object>();
-////		WSS4JInInterceptor wssIn = new WSS4JInInterceptor(inProps);
-////		cxfEndpoint.getInInterceptors().add(wssIn);
-//
-//		// process outgoing messages
-//		Map<String,Object> outProps = new HashMap<String,Object>();
-//		WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
-//		cxfEndpoint.getOutInterceptors().add(wssOut);
-//
-//		outProps.put(WSHandlerConstants.ACTION, "Timestamp Signature");
-//		// use just TimeStamp with Signature
-////		outProps.put(WSHandlerConstants.USER, "myAlias");
-//		outProps.put(WSHandlerConstants.SIGNATURE_USER, "myservicekey");
-///*		outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS,
-//			ServiceKeystorePasswordCallback.class.getName()); */
-//		outProps.put(WSHandlerConstants.SIG_PROP_FILE, "/serviceKeystore.properties");
-//		outProps.put(WSHandlerConstants.SIG_KEY_ID, "DirectReference");
-//		outProps.put(WSHandlerConstants.SIGNATURE_PARTS, "{Element}{" + WSU_NS + "}Timestamp;"
-//			+ "{Element}{http://schemas.xmlsoap.org/soap/envelope/}Body");
-//		outProps.put(WSHandlerConstants.SIG_ALGO, "http://www.w3.org/2000/09/xmldsig#rsa-sha1");
-//
-//		// register our classloader to be able to
-//		// 1. load the keystore properties
-//		// 2, set context classloader to this project
-////		bus.getExtension(ResourceManager.class).addResourceResolver(
-////			new ClassLoaderResolver(Activator.class.getClassLoader()){
-////				@Override
-////				public <T> T resolve(String resourceName, Class<T> resourceType) {
-////					// return current classloader when WSS4J looks for context classloader
-////					if("".equals(resourceName) && ClassLoader.class.equals(resourceType)){
-////						return resourceType.cast(Activator.class.getClassLoader());
-////					}
-////
-////					// try to resolve resource using the current classloader
-////					return super.resolve(resourceName, resourceType);
-////				}
-////			});
-//
-//
-//
-////		bus.getExtension(ResourceManager.class).addResourceResolver(new ResourceResolver() {
-////			@Override
-////			public <T> T resolve(String resourceName, Class<T> resourceType) {
-////				if ("".equals(resourceName) && ClassLoader.class.equals(resourceType)) {
-////					return resourceType.cast(Activator.class.getClassLoader());
-////				}
-////				return null;
-////			}
-////
-////			@Override
-////			public InputStream getAsStream(String name) {
-////				return null;
-////			}
-////		});
-////
-//
-///*
-//		Map<String, Object> outProps = new HashMap<String, Object>();
-//		outProps.put("action", "UsernameToken Timestamp Signature");
-//
-//		outProps.put("signaturePropFile", "etc/Server_Decrypt.properties");
-//		outProps.put("signatureKeyIdentifier", "DirectReference");
-//		outProps.put("signatureParts", "{Element}{" + WSU_NS + "}Timestamp;"
-//			+ "{Element}{http://schemas.xmlsoap.org/soap/envelope/}Body");
-//		outProps.put("signatureAlgorithm", "http://www.w3.org/2000/09/xmldsig#rsa-sha1");
-//
-//		impl.getOutInterceptors().add(new WSS4JOutInterceptor(outProps));
-//
-//		Map<String, Object> inProps = new HashMap<String, Object>();
-//
-//		inProps.put("action", "UsernameToken Timestamp Signature");
-//		inProps.put("passwordType", "PasswordDigest");
-//		inProps.put("passwordCallbackClass", "demo.wssec.server.UTPasswordCallback");
-//
-//		inProps.put("signaturePropFile", "etc/Server_SignVerf.properties");
-//		inProps.put("signatureKeyIdentifier", "DirectReference");
-//		inProps.put("signatureAlgorithm", "http://www.w3.org/2000/09/xmldsig#rsa-sha1");
-//
-//		impl.getInInterceptors().add(new WSS4JInInterceptor(inProps));
-//
-//		// Check to make sure that the SOAP Body and Timestamp were signed
-//		DefaultCryptoCoverageChecker coverageChecker = new DefaultCryptoCoverageChecker();
-//		impl.getInInterceptors().add(coverageChecker);
-//		*/
-//
-//	}
-
-	private CXFNonSpringServlet _cxfServlet;
-	private ServletRegistration.Dynamic _cxfServletRegistration;
+	private LiferayCXFOSGiServlet _cxfServlet;
+	private List<Endpoint> _endpoints;
+	private Server _httpServer;
+	private BundleContext _bundleContext;
 }
--- osgi/modules/jax-ws-osgi/src/main/java/com/liferay/portal/service/osgi/cxf/externalpolicy/WSDLReferenceDomainExpression.java
+++ osgi/modules/jax-ws-osgi/src/main/java/com/liferay/portal/service/osgi/cxf/externalpolicy/WSDLReferenceDomainExpression.java
@@ -4,89 +4,114 @@ import org.apache.cxf.service.model.BindingFaultInfo;
 import org.apache.cxf.service.model.BindingMessageInfo;
 import org.apache.cxf.service.model.BindingOperationInfo;
 import org.apache.cxf.service.model.EndpointInfo;
+import org.apache.cxf.service.model.InterfaceInfo;
+import org.apache.cxf.service.model.OperationInfo;
 import org.apache.cxf.service.model.ServiceInfo;
 import org.apache.cxf.ws.policy.attachment.external.DomainExpression;
 
 import javax.xml.namespace.QName;
-import java.util.regex.Pattern;
 
 /**
  * @author Tomas Polesovsky
  */
 public class WSDLReferenceDomainExpression implements DomainExpression {
-	private Pattern _endpointRegExp;
-	private QName serviceReference;
-	private QName endpointReference;
-	private QName bindingOperationReference;
-	private QName bindingMessageReference;
-	private QName bindingFaultReference;
-
-	public WSDLReferenceDomainExpression() {
-	}
 
 	@Override
 	public boolean appliesTo(BindingFaultInfo bfi) {
-		return bfi.getFaultInfo().getOperation().getInterface().getName().equals(getBindingFaultReference());
+		if (bfi == null) {
+			return false;
+		}
+
+		return appliesTo(bfi.getFaultInfo().getOperation(), getBindingFaultReference());
 	}
 
 	@Override
 	public boolean appliesTo(BindingMessageInfo bmi) {
-		return bmi.getMessageInfo().getOperation().getInterface().getName().equals(getBindingMessageReference());
+		if (bmi == null) {
+			return false;
+		}
+
+		return appliesTo(bmi.getMessageInfo().getOperation(), getBindingMessageReference());
 	}
 
 	@Override
 	public boolean appliesTo(BindingOperationInfo boi) {
-		return boi.getOperationInfo().getInterface().getName().equals(getBindingOperationReference());
+		if (boi == null) {
+			return false;
+		}
+
+		return appliesTo(boi.getOperationInfo(), getBindingOperationReference());
 	}
 
 	@Override
 	public boolean appliesTo(EndpointInfo ei) {
-//		return _endpointRegExp.matcher(ei.getAddress()).matches(); // EndpointPolicyImpl:151
-		return ei.getInterface().getName().equals(getEndpointReference());
+		if (ei == null) {
+			return false;
+		}
+
+		return appliesTo(ei.getInterface(), getEndpointReference());
 	}
 
 	@Override
 	public boolean appliesTo(ServiceInfo si) {
-		return si.getInterface().getName().equals(getServiceReference());
-	}
+		if (si == null) {
+			return false;
+		}
 
-	public void setServiceReference(QName serviceReference) {
-		this.serviceReference = serviceReference;
-	}
-
-	public QName getServiceReference() {
-		return serviceReference;
-	}
-
-	public void setEndpointReference(QName endpointReference) {
-		this.endpointReference = endpointReference;
-	}
-
-	public QName getEndpointReference() {
-		return endpointReference;
-	}
-
-	public void setBindingOperationReference(QName bindingOperationReference) {
-		this.bindingOperationReference = bindingOperationReference;
-	}
-
-	public QName getBindingOperationReference() {
-		return bindingOperationReference;
-	}
-
-	public void setBindingMessageReference(QName bindingMessageReference) {
-		this.bindingMessageReference = bindingMessageReference;
-	}
-
-	public QName getBindingMessageReference() {
-		return bindingMessageReference;
-	}
-
-	public void setBindingFaultReference(QName bindingFaultReference) {
-		this.bindingFaultReference = bindingFaultReference;
+		return appliesTo(si.getInterface(), getServiceReference());
 	}
 
 	public QName getBindingFaultReference() {
-		return bindingFaultReference;
+		return _bindingFaultReference;
 	}
+
+	public QName getBindingMessageReference() {
+		return _bindingMessageReference;
+	}
+
+	public QName getBindingOperationReference() {
+		return _bindingOperationReference;
+	}
+
+	public QName getEndpointReference() {
+		return _endpointReference;
+	}
+
+	public QName getServiceReference() {
+		return _serviceReference;
+	}
+
+	public void setBindingFaultReference(QName bindingFaultReference) {
+		this._bindingFaultReference = bindingFaultReference;
+	}
+
+	public void setBindingMessageReference(QName bindingMessageReference) {
+		this._bindingMessageReference = bindingMessageReference;
+	}
+
+	public void setBindingOperationReference(QName bindingOperationReference) {
+		this._bindingOperationReference = bindingOperationReference;
+	}
+
+	public void setEndpointReference(QName endpointReference) {
+		this._endpointReference = endpointReference;
+	}
+
+	public void setServiceReference(QName _serviceReference) {
+		this._serviceReference = _serviceReference;
+	}
+
+	protected boolean appliesTo(OperationInfo operationInfo, QName localReference) {
+		return appliesTo(operationInfo.getInterface(), localReference);
+	}
+
+	protected boolean appliesTo(InterfaceInfo interfaceInfo, QName localReference) {
+		return interfaceInfo.getName().equals(localReference);
+	}
+
+	private QName _bindingOperationReference;
+	private QName _bindingMessageReference;
+	private QName _bindingFaultReference;
+	private QName _endpointReference;
+	private QName _serviceReference;
 }
