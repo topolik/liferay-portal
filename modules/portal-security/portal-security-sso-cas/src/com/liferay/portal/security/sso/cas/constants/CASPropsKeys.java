d2c89f7874f2053d7505ac00cc35ee34521f9b23
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2015-09-23 16:29:33 +0100

    LPS-58444 Create new PropsKeys in CAS module using the configuration API format

---
+++ modules/portal-security/portal-security-sso-cas/src/com/liferay/portal/security/sso/cas/constants/CASPropsKeys.java
@@ -0,0 +1,22 @@
+package com.liferay.portal.security.sso.cas.constants;
+
+public interface CASPropsKeys {
+
+	public static final String CAS_AUTH_ENABLED = "casEnabled";
+
+	public static final String CAS_IMPORT_FROM_LDAP = "casImportFromLDAP";
+
+	public static final String CAS_LOGIN_URL = "casLoginURL";
+
+	public static final String CAS_LOGOUT_ON_SESSION_EXPIRATION = "casLogoutOnSessionExpiration";
+
+	public static final String CAS_LOGOUT_URL = "casLogoutURL";
+
+	public static final String CAS_NO_SUCH_USER_REDIRECT_URL = "casNoSuchUserRedirectURL";
+
+	public static final String CAS_SERVER_NAME = "casServerName";
+
+	public static final String CAS_SERVER_URL = "casServerURL";
+
+	public static final String CAS_SERVICE_URL = "casServiceURL";
+}
