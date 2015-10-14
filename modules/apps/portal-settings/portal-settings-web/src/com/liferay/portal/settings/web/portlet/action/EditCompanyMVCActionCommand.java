7d12f6e2414edd4b9d3f25a58bcb3149d6a2b8c9
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2015-10-14 23:07:16 +0200

    LPS-58832 Split validation phase from the save operation

--- modules/apps/portal-settings/portal-settings-web/src/com/liferay/portal/settings/web/portlet/action/EditCompanyMVCActionCommand.java
+++ modules/apps/portal-settings/portal-settings-web/src/com/liferay/portal/settings/web/portlet/action/EditCompanyMVCActionCommand.java
@@ -28,7 +28,7 @@ import com.liferay.portal.NoSuchListTypeException;
 import com.liferay.portal.NoSuchRegionException;
 import com.liferay.portal.PhoneNumberException;
 import com.liferay.portal.WebsiteURLException;
-import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseFormMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.servlet.SessionErrors;
@@ -71,7 +71,7 @@ import org.osgi.service.component.annotations.Reference;
 	},
 	service = MVCActionCommand.class
 )
-public class EditCompanyMVCActionCommand extends BaseMVCActionCommand {
+public class EditCompanyMVCActionCommand extends BaseFormMVCActionCommand {
 
 	@Override
 	public void doProcessAction(
@@ -82,16 +82,10 @@ public class EditCompanyMVCActionCommand extends BaseMVCActionCommand {
 
 		try {
 			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
-				validateCAS(actionRequest);
-				validateLDAP(actionRequest);
-				validateSocialInteractions(actionRequest);
-
 				String redirect = ParamUtil.getString(
 					actionRequest, "redirect");
 
-				if (SessionErrors.isEmpty(actionRequest)) {
-					updateCompany(actionRequest);
-				}
+				updateCompany(actionRequest);
 
 				sendRedirect(actionRequest, actionResponse, redirect);
 			}
@@ -136,6 +130,16 @@ public class EditCompanyMVCActionCommand extends BaseMVCActionCommand {
 
 			actionResponse.setRenderParameter("mvcPath", mvcPath);
 		}
+	}
+
+	@Override
+	protected void doValidateForm(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		validateCAS(actionRequest);
+		validateLDAP(actionRequest);
+		validateSocialInteractions(actionRequest);
 	}
 
 	@Reference(unbind = "-")
