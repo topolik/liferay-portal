507c4ed1a48b13f82ce4df7ffd1f6099b5ae4a4e
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-01-20 11:28:33 +0100

    If portlet doesn't exist yet, use the default individual ResourcePermission record created by PortletLocalServiceImpl.check()

--- portal-impl/src/com/liferay/portal/service/permission/PortletPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/PortletPermissionImpl.java
@@ -23,6 +23,7 @@ import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutTypePortlet;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.PortletConstants;
+import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.impl.VirtualLayout;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.ActionKeys;
@@ -31,6 +32,7 @@ import com.liferay.portal.security.permission.ResourceActionsUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.PortletLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.util.PortletCategoryKeys;
 import com.liferay.portlet.ControlPanelEntry;
 import com.liferay.portlet.exportimport.staging.permission.StagingPermissionUtil;
@@ -335,6 +337,13 @@ public class PortletPermissionImpl implements PortletPermission {
 		}
 
 		primKey = getPrimaryKey(layout.getPlid(), portletId);
+
+		if (ResourcePermissionLocalServiceUtil.getResourcePermissionsCount(
+				permissionChecker.getCompanyId(), name,
+				ResourceConstants.SCOPE_INDIVIDUAL, primKey) == 0) {
+
+			primKey = name;
+		}
 
 		if (strict) {
 			return permissionChecker.hasPermission(
