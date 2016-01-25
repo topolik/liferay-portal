cc1809804a3d529ddd28b10a363b53c67414e0ec
Julio Camarero <julio.camarero@liferay.com>
2016-01-25 15:37:14 +0100

    LPS-62335 move logic to new module

--- modules/apps/application-list/application-list-user-personal-site-permissions/src/main/java/com/liferay/application/list/user/personal/site/permissions/UserPersonalSitePermissions.java
+++ modules/apps/application-list/application-list-user-personal-site-permissions/src/main/java/com/liferay/application/list/user/personal/site/permissions/UserPersonalSitePermissions.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.application.list.permissions;
+package com.liferay.application.list.user.personal.site.permissions;
 
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.kernel.exception.PortalException;
