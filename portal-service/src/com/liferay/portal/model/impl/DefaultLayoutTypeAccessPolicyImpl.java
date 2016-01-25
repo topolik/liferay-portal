77735323d1912260828b6167f03697df176475f3
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-01-25 10:27:43 +0100

    Don't create new permission records from defaults on the fly, permission framework will use default records

--- portal-service/src/com/liferay/portal/model/impl/DefaultLayoutTypeAccessPolicyImpl.java
+++ portal-service/src/com/liferay/portal/model/impl/DefaultLayoutTypeAccessPolicyImpl.java
@@ -57,8 +57,6 @@ public class DefaultLayoutTypeAccessPolicyImpl
 		throws PortalException {
 
 		if (isAccessAllowedToLayoutPortlet(request, layout, portlet)) {
-			PortalUtil.addPortletDefaultResource(request, portlet);
-
 			if (hasAccessPermission(request, layout, portlet)) {
 				return;
 			}
