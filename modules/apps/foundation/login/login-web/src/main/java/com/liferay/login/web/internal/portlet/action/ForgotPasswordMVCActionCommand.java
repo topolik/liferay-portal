96cbb50a9a89af49b269e8d82ab0a18f3aae6c25
Tibor Lipusz <tibor.lipusz@liferay.com>
2018-02-14 18:29:38 +0100

    LPS-71001 login-web: Fix Auto SF warning about chaining

--- modules/apps/foundation/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/ForgotPasswordMVCActionCommand.java
+++ modules/apps/foundation/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/ForgotPasswordMVCActionCommand.java
@@ -131,7 +131,9 @@ public class ForgotPasswordMVCActionCommand extends BaseMVCActionCommand {
 
 				String answer = ParamUtil.getString(actionRequest, "answer");
 
-				if (!user.getReminderQueryAnswer().equals(answer)) {
+				String reminderQueryAnswer = user.getReminderQueryAnswer();
+
+				if (!reminderQueryAnswer.equals(answer)) {
 					throw new UserReminderQueryException();
 				}
 			}
