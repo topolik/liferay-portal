ec8a5336eaa9a6395e0000d8522556b4382a36f2
Carlos Sierra <carlos.sierra@liferay.com>
2018-05-31 14:56:28 +0200

    LPS-81770 SF

--- modules/apps/portal-security/portal-security-auth-verifier-test/src/main/java/com/liferay/portal/security/auth/verifier/test/internal/activator/AuthVerifierTestBundleActivator.java
+++ modules/apps/portal-security/portal-security-auth-verifier-test/src/main/java/com/liferay/portal/security/auth/verifier/test/internal/activator/AuthVerifierTestBundleActivator.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.util.HashMapDictionary;
 
 import java.io.IOException;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.List;
@@ -38,7 +39,7 @@ import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 /**
  * @author Marta Medio
  */
-public class JaxServletBundleActivator implements BundleActivator {
+public class AuthVerifierTestBundleActivator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext bundleContext) {
@@ -130,12 +131,13 @@ public class JaxServletBundleActivator implements BundleActivator {
 			throws IOException {
 
 			String remoteUser = request.getRemoteUser();
+			PrintWriter writer = response.getWriter();
 
 			if (remoteUser == null) {
-				response.getWriter().write("");
+				writer.write("");
 			}
 			else {
-				response.getWriter().write(remoteUser);
+				writer.write(remoteUser);
 			}
 		}
 
