2e4221ba04bddfe0f99d649b229e2d67f9ee65f3
Marta Medio <marta.medio@liferay.com>
2018-07-06 12:34:52 +0200

    OAUTH2-207 Set CORS headers on token requests

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/LiferayAccessTokenService.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/LiferayAccessTokenService.java
@@ -15,16 +15,27 @@
 package com.liferay.oauth2.provider.rest.internal.endpoint.access.token;
 
 import com.liferay.oauth2.provider.rest.internal.endpoint.constants.OAuth2ProviderRestEndpointConstants;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.rs.security.oauth2.common.Client;
@@ -35,6 +46,49 @@ import org.apache.cxf.rs.security.oauth2.services.AccessTokenService;
  */
 @Path("/token")
 public class LiferayAccessTokenService extends AccessTokenService {
+
+	@Consumes("application/x-www-form-urlencoded")
+	@Override
+	@POST
+	@Produces("application/json")
+	public Response handleTokenRequest(MultivaluedMap<String, String> params) {
+		Response response = super.handleTokenRequest(params);
+
+		Client client = authenticateClientIfNeeded(params);
+
+		MultivaluedMap<String, Object> headers = response.getHeaders();
+
+		String origin = _httpHeaders.getHeaderString("Origin");
+
+		if (Validator.isBlank(origin)) {
+			return response;
+		}
+
+		for (String redirect : client.getRedirectUris()) {
+			try {
+				URI originURI = new URI(origin);
+				URI redirectURI = new URI(redirect);
+
+				String originHost = originURI.getHost();
+				String redirectHost = redirectURI.getHost();
+
+				if (originHost.equals(redirectHost)) {
+					headers.putSingle(
+						"Accept", "application/x-www-form-urlencoded");
+					headers.putSingle("Access-Control-Allow-Methods", "POST");
+					headers.putSingle("Access-Control-Allow-Origin", origin);
+					headers.putSingle("Content-Type", "application/json");
+				}
+			}
+			catch (URISyntaxException urise) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("Invalid callback uri", urise);
+				}
+			}
+		}
+
+		return response;
+	}
 
 	@Override
 	protected Client authenticateClientIfNeeded(
@@ -70,5 +124,11 @@ public class LiferayAccessTokenService extends AccessTokenService {
 
 		return client;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LiferayAccessTokenService.class);
+
+	@Context
+	private HttpHeaders _httpHeaders;
 
 }
