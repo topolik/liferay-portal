topolik/liferay-portal

commit 22aa37e4375d3438e673a346281740c8756d0491

LPS-82191 Hugo SF (please enforce, read whole comment, thx), Remove arrow functions in jsp files.
.jsp files are not transpiled and this was creating errors in browsers
that do not support ES6 (IE 11).

modules/apps/login/login-web/src/main/resources/META-INF/resources/login_redirect.jsp
@@ -94,15 +94,15 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 						method: 'POST'
 					}
 				).then(
-					response => {
+					function(response) {
 						return response.ok ? response.json() : Promise.reject();
 					}
 				).then(
-					data => {
+					function(data) {
 						return !data.exception ? data.userStatus : Promise.reject();
 					}
 				).then(
-					userStatus => {
+					function(userStatus) {
 						var message = '';
 
 						if (userStatus == 'user_added') {

modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/view_fragment_entries.jsp
@@ -173,7 +173,7 @@
 		}
 
 		Liferay.componentReady('fragmentEntriesManagementToolbar').then(
-			(managementToolbar) => {
+			function(managementToolbar) {
 				managementToolbar.on('creationButtonClicked', handleAddFragmentEntryMenuItemClick);
 			}
 		);
@@ -199,7 +199,7 @@
 	};
 
 	Liferay.componentReady('fragmentEntriesManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on(
 				'actionItemClicked',
 					function(event) {

modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/view_fragment_entry_usages.jsp
@@ -184,7 +184,7 @@ renderResponse.setTitle(LanguageUtil.format(request, "usages-and-propagation-x",
 	};
 
 	Liferay.componentReady('fragmentEntryLinksManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on(
 				'actionItemClicked',
 				function(event) {

modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/view_display_pages.jsp
@@ -274,7 +274,7 @@ DisplayPageDisplayContext displayPageDisplayContext = new DisplayPageDisplayCont
 	};
 
 	Liferay.componentReady('displayPagesManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on('creationButtonClicked', handleAddDisplayPageMenuItemClick);
 
 			managementToolbar.on(

modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/view_layout_page_template_entries.jsp
@@ -199,7 +199,7 @@ request.setAttribute(LayoutAdminWebKeys.LAYOUT_PAGE_TEMPLATE_DISPLAY_CONTEXT, la
 	};
 
 	Liferay.componentReady('layoutPageTemplateEntriesManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on(
 				['actionItemClicked', 'creationMenuItemClicked', 'filterItemClicked'],
 				function(event) {

modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/view_layout_prototypes.jsp
@@ -121,7 +121,7 @@
 	};
 
 	Liferay.componentReady('layoutPrototypeManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on(
 				['actionItemClicked', 'filterItemClicked'],
 				function(event) {

modules/apps/password-policies-admin/password-policies-admin-web/src/main/resources/META-INF/resources/edit_password_policy_assignments.jsp
@@ -175,7 +175,7 @@ PortletURL portletURL = editPasswordPolicyAssignmentsManagementToolbarDisplayCon
 	}
 
 	Liferay.componentReady('editPasswordPolicyAssignmentsManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on('creationButtonClicked', addAssignees);
 		}
 	);

modules/apps/roles/roles-admin-web/src/main/resources/META-INF/resources/edit_role_assignments.jsp
@@ -168,7 +168,7 @@ renderResponse.setTitle(role.getTitle(locale));
 	}
 
 	Liferay.componentReady('editRoleAssignmentsManagementToolbar').then(
-		(managementToolbar) => {
+		function(managementToolbar) {
 			managementToolbar.on('creationButtonClicked', addAssignees);
 		}
 	);

commit dc145385969953d1acd374318b77d28e415b6ced

LPS-83431 Fix regression caused by 22aa37e43.
The JSP logic does not only relate to anonymous accounts handling. Web redirect based SSOs use this JSP to call the afterLogin() function on the caller window and to close the window

modules/apps/login/login-web/src/main/resources/META-INF/resources/login_redirect.jsp
@@ -128,6 +128,11 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 			}
 		);
 
+	</aui:script>
+</c:if>
+
+	<aui:script sandbox="<%= true %>">
+
 		window.<portlet:namespace />closeDialog = function() {
 			var namespace = window.parent.namespace;
 
@@ -142,9 +147,8 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 		<c:if test="<%= !company.isStrangers() && (user == null) %>">
 			<portlet:namespace />closeDialog();
 		</c:if>
-	</aui:script>
 
-	<aui:script sandbox="<%= true %>">
+
 		var afterLogin;
 		var namespace;
 		var randomNamespace;
@@ -193,4 +197,3 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 			}
 		}
 	</aui:script>
-</c:if>