topolik/liferay-portal

commit 983123c249c24c9235a7fca932bbdbd94fd316f9

LPS-81346 - Remove jQuery in Login portlet

modules/apps/login/login-web/src/main/resources/META-INF/resources/login_redirect.jsp
@@ -45,50 +45,77 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 </c:if>
 
 <aui:script>
-	function <portlet:namespace />activateAccount() {
-		var $ = AUI.$;
+	var <portlet:namespace />activateAccount = Liferay.lazyLoad(
+		'metal-dom/src/dom',
+		function(dom) {
+			var form = document.getElementById('<portlet:namespace />fm');
 
-		var form = $(document.<portlet:namespace />fm);
+			function onError() {
+				var message = '<liferay-ui:message key="your-request-failed-to-complete" />';
 
-		function onError() {
-			var message = '<liferay-ui:message key="your-request-failed-to-complete" />';
+				<portlet:namespace />showStatusMessage('danger', message);
 
-			<portlet:namespace />showStatusMessage('danger', message);
+				var anonymousAccount = form.querySelector('.anonymous-account');
 
-			$('.anonymous-account').addClass('hide');
-		}
+				if (anonymousAccount) {
+					dom.addClasses(anonymousAccount, 'hide');
 
-		form.ajaxSubmit(
-			{
-				dataType: 'json',
-				error: onError,
-				success: function(responseData) {
-					var exception = responseData.exception;
-
-					var message;
-
-					if (!exception) {
-						var userStatus = responseData.userStatus;
-
-						if (userStatus == 'user_added') {
-
-							message = '<liferay-ui:message arguments="<%= emailAddress %>" key="thank-you-for-creating-an-account-your-password-was-sent-to-x" translateArguments="<%= false %>" />';
-						}
-						else if (userStatus == 'user_pending') {
-							message = '<liferay-ui:message arguments="<%= emailAddress %>" key="thank-you-for-creating-an-account.-you-will-be-notified-via-email-at-x-when-your-account-has-been-approved" translateArguments="<%= false %>" />';
-						}
-
-						<portlet:namespace />showStatusMessage('success', message);
-
-						$('.anonymous-account').addClass('hide');
-					}
-					else {
-						onError();
-					}
+					dom.removeClasses(anonymousAccount, 'show');
 				}
 			}
-		);
-	}
+
+			fetch(
+				'<%= updateIncompleteUserURL %>',
+				{
+					body: new FormData(form);,
+					credentials: 'include',
+					headers: new Headers({
+						'Content-Type': 'application/json'
+					}),
+					method: 'POST'
+				}
+			).then(
+				response => {
+					if (!response.ok) {
+						onError();
+					}
+					else {
+						response.json().then(
+							function(data) {
+								var exception = data.exception;
+
+								var message;
+
+								if (!exception) {
+									var userStatus = data.userStatus;
+
+									if (userStatus == 'user_added') {
+										message = '<liferay-ui:message arguments="<%= emailAddress %>" key="thank-you-for-creating-an-account-your-password-was-sent-to-x" translateArguments="<%= false %>" />';
+									}
+									else if (userStatus == 'user_pending') {
+										message = '<liferay-ui:message arguments="<%= emailAddress %>" key="thank-you-for-creating-an-account.-you-will-be-notified-via-email-at-x-when-your-account-has-been-approved" translateArguments="<%= false %>" />';
+									}
+
+									<portlet:namespace />showStatusMessage('success', message);
+
+									var anonymousAccount = document.querySelector('.anonymous-account');
+
+									if (anonymousAccount) {
+										dom.addClasses(anonymousAccount, 'hide');
+
+										dom.removeClasses(anonymousAccount, 'show');
+									}
+								}
+								else {
+									onError();
+								}
+							}
+						);
+					}
+				}
+			);
+		}
+	);
 
 	function <portlet:namespace />closeDialog() {
 		var namespace = window.parent.namespace;
@@ -101,15 +128,23 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 		);
 	}
 
-	function <portlet:namespace />showStatusMessage(type, message) {
-		var messageContainer = AUI.$('#<portlet:namespace />login-status-messages');
+	var <portlet:namespace />showStatusMessage = Liferay.lazyLoad(
+		'metal-dom/src/dom',
+		function(dom, type, message) {
+			var messageContainer = document.getElementById('<portlet:namespace />login-status-messages');
 
-		messageContainer.removeClass('alert-danger').removeClass('alert-success');
+			if (messageContainer) {
+				dom.removeClasses(messageContainer, 'alert-danger');
+				dom.removeClasses(messageContainer, 'alert-success');
 
-		messageContainer.addClass('alert alert-' + type);
+				dom.addClasses(messageContainer, 'alert alert-' + type);
 
-		messageContainer.html(message).removeClass('hide');
-	}
+				messageContainer.innerHTML(message);
+
+				dom.removeClasses(messageContainer, 'hide');
+			}
+		}
+	);
 
 	<c:if test="<%= !company.isStrangers() && (user == null) %>">
 		<portlet:namespace />closeDialog();

modules/apps/login/login-web/src/main/resources/META-INF/resources/login.jsp
@@ -182,32 +182,38 @@
 			<%@ include file="/navigation.jspf" %>
 		</div>
 
-		<aui:script sandbox="<%= true %>">
-			var form = AUI.$(document.<portlet:namespace /><%= formName %>);
+		<aui:script>
+			var <portlet:namespace />form = document.getElementById('<portlet:namespace /><%= formName %>');
 
-			form.on(
-				'submit',
-				function(event) {
-					<c:if test="<%= Validator.isNotNull(redirect) %>">
-						var redirect = form.fm('redirect');
+			if (<portlet:namespace />form) {
+				<portlet:namespace />form.addEventListener(
+					'submit',
+					function(event) {
+						<c:if test="<%= Validator.isNotNull(redirect) %>">
+							var redirect = <portlet:namespace />form.querySelector('#<portlet:namespace />redirect');
 
-						if (redirect) {
-							var redirectVal = redirect.val();
+							if (redirect) {
+								var redirectVal = redirect.getAttribute('value');
 
-							redirect.val(redirectVal + window.location.hash);
+								redirect.setAttribute('value', redirectVal + window.location.hash);
+							}
+						</c:if>
+
+						submitForm(<portlet:namespace />form);
+					}
+				);
+
+				var <portlet:namespace />password = <portlet:namespace />form.querySelector('#<portlet:namespace />password');
+
+				if (<portlet:namespace />password) {
+					<portlet:namespace />password.addEventListener(
+						'keypress',
+						function(event) {
+							Liferay.Util.showCapsLock(event, '<portlet:namespace />passwordCapsLockSpan');
 						}
-					</c:if>
-
-					submitForm(form);
+					);
 				}
-			);
-
-			form.fm('password').on(
-				'keypress',
-				function(event) {
-					Liferay.Util.showCapsLock(event, '<portlet:namespace />passwordCapsLockSpan');
-				}
-			);
+			}
 		</aui:script>
 	</c:otherwise>
 </c:choose>

commit b4c8b5434428c8f995153349a4ed0838526f8ebf

LPS-82716 innerHTML is not a function (regression in 983123c2)

modules/apps/login/login-web/src/main/resources/META-INF/resources/login_redirect.jsp
@@ -55,7 +55,7 @@ boolean anonymousAccount = ParamUtil.getBoolean(request, "anonymousUser");
 
 					dom.addClasses(messageContainer, 'alert alert-' + type);
 
-					messageContainer.innerHTML(message);
+					messageContainer.innerHTML = message;
 
 					dom.removeClasses(messageContainer, 'hide');
 				}