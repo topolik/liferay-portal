c55c95a628dbe348401fbdbd9333111ad0b1bd06
arthurchan35 <arthurchan35@gmail.com>
2019-01-11 18:25:20 +0800

    LPS-86515 skeleton of a TOTPPortlet

---
+++ modules/apps/multi-factor-authentication/multi-factor-authentication-totp-web/src/main/java/com/liferay/multi/factor/authentication/totp/web/internal/portlet/action/TOTPVerificationActionCommand.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.multi.factor.authentication.totp.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author arthurchan35
+ */
+@Component(
+	property = {
+		"javax.portlet.name=TOTPPortlet", "mvc.command.name=/totp/verify"
+	},
+	service = MVCActionCommand.class
+)
+public class TOTPVerificationActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+	}
+
+}
