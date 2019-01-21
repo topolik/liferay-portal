358d91df8da7d230eb9ab085784274541fa3066d
arthurchan35 <arthurchan35@gmail.com>
2019-01-21 15:43:33 +0800

    LPS-86516 auto gen

--- modules/apps/multi-factor-authentication/multi-factor-authentication-otp-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/multi-factor-authentication/multi-factor-authentication-otp-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,3 +1,11 @@
+create table HOTP (
+	hotpId LONG not null primary key,
+	userId LONG,
+	count LONG,
+	sharedSecret VARCHAR(75) null,
+	verified BOOLEAN
+);
+
 create table TOTP (
 	totpId LONG not null primary key,
 	userId LONG,
