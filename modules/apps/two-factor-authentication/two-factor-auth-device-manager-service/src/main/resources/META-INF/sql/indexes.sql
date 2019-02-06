67167abe3a8253950c9ca27faf28583ff3343cbb
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-02-06 18:52:58 +0100

    Remote old GS code

--- modules/apps/two-factor-authentication/two-factor-auth-device-manager-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,6 +0,0 @@
-create index IX_C28AA5FE on Bemis_Device (portalUserId, deviceIP[$COLUMN_LENGTH:75$]);
-create index IX_83DCE0BC on Bemis_Device (tempDevice, portalUserId, deviceIP[$COLUMN_LENGTH:75$]);
-create index IX_951B1CFA on Bemis_Device (verified, portalUserId, deviceIP[$COLUMN_LENGTH:75$]);
-
-create index IX_4A2B5580 on Bemis_DeviceCode (emailAddress[$COLUMN_LENGTH:75$]);
-create index IX_1A70381A on Bemis_DeviceCode (portalUserId);
