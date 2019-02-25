6909aa68758aa903618ba76724608a12cdf433b5
Marta Medio <marta.medio@liferay.com>
2019-02-25 21:33:55 +0100

    MFA - Add Category and Entries for "Account Settings"

---
+++ modules/apps/multi-factor-authentication/multi-factor-authentication-portlet-web/src/main/java/com/liferay/multi/factor/authentication/portlet/web/internal/user/settings/MFAScreenNavigationCategory.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.multi.factor.authentication.portlet.web.internal.user.settings;
+
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.multi.factor.authentication.portlet.api.constants.MFAPortletKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.users.admin.constants.UserFormConstants;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marta Medio
+*/
+@Component(
+	property = {"screen.navigation.category.order:Integer=40"},
+	service = {ScreenNavigationCategory.class}
+)
+public class MFAScreenNavigationCategory implements ScreenNavigationCategory {
+
+	@Override
+	public String getCategoryKey() {
+		return MFAPortletKeys.CATEGORY_KEY_MFA;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, getCategoryKey());
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return UserFormConstants.SCREEN_NAVIGATION_KEY_USERS;
+	}
+
+}
