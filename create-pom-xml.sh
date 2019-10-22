24c2a64865fbf7d437ad01fbc2eeb7df58cea25b
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-10-22 11:49:31 +0200

    Honor only compileInclude

--- create-pom-xml.sh
+++ create-pom-xml.sh
@@ -5,11 +5,11 @@ find -name 'build.gradle' | (while read buildFile; do
     DIR=$(dirname "$buildFile")
     DIR_NAME=$(basename "$DIR")
 
-    grep "compile[a-zA-Z]* group" "$buildFile" > /dev/null || continue
+    grep "compileInclude group" "$buildFile" > /dev/null || continue
 
     # compileOnly group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
     IFS=$'\n'
-    for dep in $(grep "compile[a-zA-Z]* group" "$buildFile"); do
+    for dep in $(grep "compileInclude group" "$buildFile"); do
         GROUP=$(echo "$dep" | cut -d '"' -f 2)
         NAME=$(echo "$dep" | cut -d '"' -f 4)
         VERSION=$(echo "$dep" | cut -d '"' -f 6)
@@ -31,11 +31,9 @@ cat > "pom.xml" <<EOF
     <version>master</version>
     <packaging>jar</packaging>
     <dependencies>
-EOF
 
-cat dependencies.xml | sort -u >> "pom.xml"
+$(cat dependencies.xml | sort -u)
 
-cat >> "pom.xml" <<EOF
     </dependencies>
 </project>
 EOF
